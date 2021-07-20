26339ca671def7237000954cbecdbd32d906ff51
SteveNoyes <stevenmnoyes.af@gmail.com>
2021-07-20 17:45:47 -0500

    first commit

---
+++ javaScript.js
@@ -0,0 +1,21 @@
+// wrapper
+//
+// heading
+//
+// graph
+//
+// fname
+//
+// lname
+
+document.getElementById('heading').innerHTML = "Lorem Ipsum,";
+
+function testing() {
+  document.getElementById('heading').style.color = 'red';
+}
+
+
+
+var myVar = 1 + 1;
+
+console.log(myVar);
